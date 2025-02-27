5dcbd8ab51ef6c5bc95c547b1ae7cb146fadfd65
Engelbert107 <etchinde@gmail.com>
2025-02-26 23:08:55 -0500

    renove library for random search

--- ml_helper/inference.py
+++ ml_helper/inference.py
@@ -2,6 +2,34 @@ import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences # type: ignore
 
 
+
+def predict_sentiment_base(model, vectorizer, text, class_labels):
+    """
+    Predict the sentiment of a given text using a trained model and vectorizer (TF-IDF vectorizer).
+
+    Parameters:
+        model: Trained classifier (e.g., LinearSVC)
+        vectorizer: Trained TF-IDF vectorizer
+        text: A string or list of strings containing the input text
+        class_labels: A list mapping encoded labels to sentiment categories
+
+    Returns:
+        Predicted sentiment class
+    """
+    # Ensure input is a list
+    if isinstance(text, str):
+        text = [text]
+    
+    # Transform text using the loaded vectorizer
+    text_tfidf = vectorizer.transform(text)
+    
+    # Predict using the trained model
+    predicted_label = model.predict(text_tfidf)
+    
+    # Map the predicted label to the class name
+    return class_labels[predicted_label[0]]
+
+
 def predict_sentiment(model, tokenizer, text, max_length, class_labels):
     """
     Predict the sentiment of a given text using the trained BiLSTM model.
